a410d39ab16a87be6cbd374331bd56e8bc672e25
Hong Phuc <hongphuc.dthp@gmail.com>
2022-02-15 23:12:45 +0700

    update script

--- script.py
+++ script.py
@@ -1,24 +1,38 @@
 
 from pathlib import Path
-import os, time
+import os, time, datetime
 
-# Input file name
-file_name = input("Enter your file name: ")
+# Input file date
+# Date format: dd/mm/yyyy
+file_date = input("Enter your datetime: ")
 
 parent_dir = '/Users/hongphuc/Documents/PLAYGROUND/python-script-playground'
 
 # ** = all sub-directories, *.txt = all files with .txt extension
-matching_file_pattern = ('**/{}.txt').format(file_name)
+# matching_file_pattern = ('**/*.txt').format(file_name)
 
-for file in Path(parent_dir).glob(matching_file_pattern):
-    print(file.parent)
+for file in Path(parent_dir).glob('**/*.txt'):
+    print("=====")
+    print("File path: {}".format(file))
 
-    # Create target directory if not exists
-    # target_dir = '/Users/hongphuc/Documents/PLAYGROUND/python-script-playground-backup'
-    target_dir = str(file.parent).replace("python-script-playground", "python-script-playground-backup")
-    print(target_dir)
-    Path(target_dir).mkdir(parents=True, exist_ok=True)
+    file_created_at = datetime.datetime.fromtimestamp(os.path.getctime(file))
+    file_created_at_date = file_created_at.strftime('%d/%m/%Y')
+    print("File created at: {}".format(file_created_at_date))
 
-    # Copy file to target directory
-    new_file_path = Path(target_dir + '/' + file.name)
-    Path(file).rename(new_file_path)
+    if file_created_at_date == file_date:
+      print("File found")
+
+      # Create target directory if not exists
+      target_dir = '/Users/hongphuc/Documents/PLAYGROUND/python-script-playground-backup'
+      target_dir = str(file.parent).replace("python-script-playground", "python-script-playground-backup")
+      Path(target_dir).mkdir(parents=True, exist_ok=True)
+
+      # Copy file to target directory
+      new_file_path = Path(target_dir + '/' + file.name)
+      Path(file).rename(new_file_path)
+      print("File copied to: {}".format(new_file_path))
+    else:
+      print("File not found")
+
+    print("====")
+    print()
